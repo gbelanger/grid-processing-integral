1380b3b4e139691904518053b8f22ef592c19b30
G Belanger <gkalibra@gmail.com>
2022-10-28 16:56:16 +0200

    Moved common variables to grid.setenv.sh

--- config/grid.setenv.sh
+++ config/grid.setenv.sh
@@ -4,13 +4,12 @@ export BIN_DIR="/home/int/intportalowner/bin"
 export INT_DIR="/home/int/intportalowner/integral"
 export INTBIN_DIR="${INT_DIR}/bin"
 export SKYGRID_DIR="${INT_DIR}/skygrid"
+export OSA_DIR="${INT_DIR}/osa_support"
+export PIPELINE_DIR="${INT_DIR}/pipeline"
 export ISOC5="/data/int/isoc5/intportalowner/isocArchive"
 
 export JAVA_HOME="/home/int/intportalowner/jdk"
 export JAVA="${JAVA_HOME}/bin/java -Xms500m -Xmx500m"
-export OSA_DIR="${INT_DIR}/osa_support"
-export PIPELINE_DIR="${INT_DIR}/pipeline"
-
 export CALC="${BIN_DIR}/calc.pl"
 
 export HEADAS="/opt/sw/heasoft6.25/x86_64-pc-linux-gnu-libc2.12"
@@ -19,3 +18,4 @@ export FTOOLS="${HEADAS}/bin"
 export HEADASNOQUERY=
 export HEADASPROMPT=/dev/null
 
+#export PFILES="${PFILES}:/home/int/intportalowner/pfiles"
